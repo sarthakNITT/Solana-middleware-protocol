b07ef2a9422f9eaf254dc9927cd9ec40e1501e87
sarthakNITT <sarthakkarodework@gmail.com>
2026-04-02 03:40:37 +0530

    checked rpc-client send and status

--- packages/rpc-client/src/send.ts
+++ packages/rpc-client/src/send.ts
@@ -1,5 +1,13 @@
 import type { RpcEndpoint, SerializedTx, Signature } from "@repo/types/index";
+import {
+    Connection,
+    VersionedTransaction,
+    clusterApiUrl
+} from "@solana/web3.js";
 
-export function sendTx(tx: SerializedTx, rpc: RpcEndpoint): Signature {
-    return "hello"
+export async function sendTx(tx: SerializedTx, rpc: RpcEndpoint): Promise<string> {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    let deserializedTx = VersionedTransaction.deserialize(tx);
+    let sig = await connection.sendTransaction(deserializedTx);
+    return JSON.stringify(sig);
 }
--- packages/rpc-client/src/status.ts
+++ packages/rpc-client/src/status.ts
@@ -1,5 +1,27 @@
 import { Signature, TxStatus } from "@repo/types/index";
+import {
+    Connection,
+    clusterApiUrl
+} from "@solana/web3.js";
 
-export function getTxStatus(signature: Signature): TxStatus {
-    return "pending"
+export async function getTxStatus(signature: Signature): Promise<TxStatus> {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const response = await connection.getSignatureStatus(signature);
+    const err = response.value?.err;
+    if (err) {
+        return "failed";
+    }
+    const status = response.value?.confirmationStatus;
+    if (status === "processed") {
+        return "pending";
+    }
+    else if (status === "confirmed" || "finalised") {
+        return "confirmed";
+    }
+    else if (!status) {
+        return "pending";
+    }
+    else {
+        return "failed";
+    }
 }
